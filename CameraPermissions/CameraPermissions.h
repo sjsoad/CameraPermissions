3a92cd727af7b4e68a845724dc47d01a6d05c3de
Sergey Kostyan <>
2018-04-26 21:18:39 +0300

    move to open

---
+++ CameraPermissions/CameraPermissions.h
@@ -0,0 +1,19 @@
+//
+//  CameraPermissions.h
+//  CameraPermissions
+//
+//  Created by Sergey Kostyan on 24.04.2018.
+//  Copyright © 2018 Sergey Kostyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraPermissions.
+FOUNDATION_EXPORT double CameraPermissionsVersionNumber;
+
+//! Project version string for CameraPermissions.
+FOUNDATION_EXPORT const unsigned char CameraPermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraPermissions/PublicHeader.h>
+
+
---
+++ CameraPermissions/Sources/CameraPermissions.swift
@@ -0,0 +1,34 @@
+//
+//  CameraServicePermissions.swift
+//  SKUtilsSwift
+//
+//  Created by Sergey Kostyan on 01.10.16.
+//  Copyright © 2016 Sergey Kostyan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import SKServicePermissions
+
+open class CameraPermissions: NSObject {
+    
+}
+
+// MARK: - ServicePermissions -
+
+extension CameraPermissions: ServicePermissions {
+    
+    public typealias PermissionsState = AVAuthorizationStatus
+    
+    public func requestPermissions(handler: @escaping (PermissionsState) -> Void) {
+        AVCaptureDevice.requestAccess(for: .video) { [weak self] _ in
+            guard let strongSelf = self else { return }
+            handler(strongSelf.permissionsState())
+        }
+    }
+    
+    public func permissionsState() -> PermissionsState {
+        return AVCaptureDevice.authorizationStatus(for: .video)
+    }
+    
+}
